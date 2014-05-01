2a16cc58d25ec7c98df201caf10da15b3febc774
Ruben Nielsen <nielsen.ruben@gmail.com>
2014-05-01 12:34:20 +0200

    Adds test to LanguageResource. Sorry for omitting it

---
+++ src/test/LanguageResourceTest.java
@@ -0,0 +1,32 @@
+package test;
+
+
+import dice.Dice;
+import internationalization.LanguageResource;
+import org.junit.Test;
+import sun.reflect.LangReflectAccess;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+
+public class LanguageResourceTest
+{
+
+	@Test
+	public void test()
+	{
+        String translation;
+
+        // Test danish
+        LanguageResource.setLocale("da", "DK");
+        translation = LanguageResource.getText("test");
+        assertEquals("Så er der fest!", translation);
+
+        // Test english
+        LanguageResource.setLocale("en", "US");
+        translation = LanguageResource.getText("test");
+        assertEquals("Now, there is a party", translation);
+	}
+
+}
