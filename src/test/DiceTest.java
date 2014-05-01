5a4093f129d0c9a52964ad666f99e34b1df17de0
thor <thor@172.16.30.72>
2014-05-01 11:19:15 +0200

    Adds dice class and test.

---
+++ src/test/DiceTest.java
@@ -0,0 +1,36 @@
+package test;
+
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import dice.Dice;
+
+
+public class DiceTest
+{
+
+	@Test
+	public void test()
+	{
+		Dice dice = new Dice();
+		int sum = 0;
+
+		for (int i = 0; i < 10000000; i++)
+		{
+			int value = dice.roll();
+			if (value < 1 || value > 6)
+			{
+				fail("dice roll not valid D6 value");
+			}
+			sum += value;
+		}
+		double average = sum / 10000000.0;
+		if (average < 3 || average > 4)
+		{
+			fail("average dice roll not valid D6 value");
+		}
+	}
+
+}
