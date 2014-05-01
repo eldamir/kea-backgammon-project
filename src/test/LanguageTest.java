f1e0bf718a184c721bbd99d3eb774911b157eefd
Rami <muhammedbrhanrami@gmail.com>
2014-05-01 13:15:31 +0200

    test language translation

---
+++ src/test/LanguageTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import internationalization.LanguageResource;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class LanguageTest 
+{
+
+	@Test
+	public void getTest() 
+	{
+		String test1 = "SÃ¥ er der fest!";
+		
+		String value = LanguageResource.getText("test");
+		
+		assertEquals(test1,value);
+	}
+	
+	@Test
+    public void setLocaleLanguage()
+	{
+		String test2 = "Now, there is a party";
+		
+		LanguageResource.setLocale("en","US");
+		
+		String val = LanguageResource.getText("test");
+		
+		assertEquals(test2,val);
+	}
+
+}
