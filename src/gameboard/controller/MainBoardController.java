e2d97ea3cebbcd35c8a4db1b41511fecd9a367cc
jonas Illum <illum@xpProject.kea>
2014-05-02 12:19:47 +0200

    Adds grid to the gameboard

--- src/gameboard/controller/MainBoardController.java
+++ src/gameboard/controller/MainBoardController.java
@@ -1,6 +1,117 @@
 package gameboard.controller;
 
-public class MainBoardController
-{
-	
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+
+public class MainBoardController {
+
+	// End homes for pieces
+	@FXML
+	private VBox blackHome;
+
+	@FXML
+	private VBox whiteHome;
+
+	// The 4 Groups of spikes
+	@FXML
+	private HBox pieceChamberNE;
+
+	@FXML
+	private HBox pieceChamberSE;
+
+	@FXML
+	private HBox pieceChamperNW;
+
+	@FXML
+	private HBox pieceChamperSW;
+
+
+
+	@FXML
+	private AnchorPane prisonField;
+
+
+	// Spikeboxes
+	@FXML
+	private VBox spikeNE19;
+
+	@FXML
+	private VBox spikeNE20;
+
+	@FXML
+	private VBox spikeNE21;
+
+	@FXML
+	private VBox spikeNE22;
+
+	@FXML
+	private VBox spikeNE23;
+
+	@FXML
+	private VBox spikeNE24;
+
+	@FXML
+	private VBox spikeNW13;
+
+	@FXML
+	private VBox spikeNW14;
+
+	@FXML
+	private VBox spikeNW15;
+
+	@FXML
+	private VBox spikeNW16;
+
+	@FXML
+	private VBox spikeNW17;
+
+	@FXML
+	private VBox spikeNW18;
+
+	@FXML
+	private VBox spikeSE1;
+
+	@FXML
+	private VBox spikeSE2;
+
+	@FXML
+	private VBox spikeSE3;
+
+	@FXML
+	private VBox spikeSE4;
+
+	@FXML
+	private VBox spikeSE5;
+
+	@FXML
+	private VBox spikeSE6;
+
+	@FXML
+	private VBox spikeSW10;
+
+	@FXML
+	private VBox spikeSW11;
+
+	@FXML
+	private VBox spikeSW12;
+
+	@FXML
+	private VBox spikeSW7;
+
+	@FXML
+	private VBox spikeSW8;
+
+	@FXML
+	private VBox spikeSW9;
+
+
+	@FXML
+	void initialize() {
+
+	}
+
+
 }
