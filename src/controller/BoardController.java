c444229a941225db98fd5b9218b602e6fbf382ab
Jonas Illum <jonas.illum@gmail.com>
2014-05-12 07:55:18 +0200

    Improves moving of pieces in GUI
    Pieces can now be dragged from one spike to another.
    Flickering and dead zones errors fixed.
    Works in PieceController, BoardController and gameboard.fxml

--- src/controller/BoardController.java
+++ src/controller/BoardController.java
@@ -1,156 +1,163 @@
 package controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Collections;
+import java.util.Map;
 
-import model.piece.Piece;
 import javafx.fxml.FXML;
-import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
+
+
+import model.board.BoardState;
+import model.piece.Piece;
 
 
 public class BoardController 
 {
+    // Game fields position assignment
+    private static final int FIRST_SPIKE_NR = 0;
+    private static final int LAST_SPIKE_NR = 23;
+    private static final int FIRST_TOP_SPIKE_NR = 12;
+    private static final int BLACK_PRISON = 24;
+    private static final int WHITE_PRISON = 25;
+    private static final int BLACK_HOME = 26;
+    private static final int WHITE_HOME = 27;
+    
+    private static final int SPIKE_HEIGHT = 5;
+    private static final int SPIKE_WIDTH = 6;
 
-	// End homes for pieces
-	@FXML
-	private VBox blackHome;
+    // spikes
+    @FXML
+    private GridPane spikesNE;
+    @FXML
+    private GridPane spikesNW;
+    @FXML
+    private GridPane spikesSE;
+    @FXML
+    private GridPane spikesSW;
 
-	@FXML
-	private VBox whiteHome;
+    // Prisons
+    @FXML
+    private AnchorPane blackPrison;
+    @FXML
+    private AnchorPane whitePrison;
+    
+    // Homes
+    @FXML
+    private AnchorPane blackHome;
+    @FXML
+    private AnchorPane whiteHome;
+    
 
-	// The 4 Groups of spikes
-	@FXML
-	private HBox pieceChamberNE;
-
-	@FXML
-	private HBox pieceChamberSE;
-
-	@FXML
-	private HBox pieceChamberNW;
-
-	@FXML
-	private HBox pieceChamberSW;
-
-
-
-	@FXML
-	private AnchorPane prisonField;
-
-
-	// Spikeboxes
-	@FXML
-	private VBox spikeNE19;
-
-	@FXML
-	private VBox spikeNE20;
-
-	@FXML
-	private VBox spikeNE21;
-
-	@FXML
-	private VBox spikeNE22;
-
-	@FXML
-	private VBox spikeNE23;
-
-	@FXML
-	private VBox spikeNE24;
-
-	@FXML
-	private VBox spikeNW13;
-
-	@FXML
-	private VBox spikeNW14;
-
-	@FXML
-	private VBox spikeNW15;
-
-	@FXML
-	private VBox spikeNW16;
-
-	@FXML
-	private VBox spikeNW17;
-
-	@FXML
-	private VBox spikeNW18;
-
-	@FXML
-	private VBox spikeSE1;
-
-	@FXML
-	private VBox spikeSE2;
-
-	@FXML
-	private VBox spikeSE3;
-
-	@FXML
-	private VBox spikeSE4;
-
-	@FXML
-	private VBox spikeSE5;
-
-	@FXML
-	private VBox spikeSE6;
-
-	@FXML
-	private VBox spikeSW10;
-
-	@FXML
-	private VBox spikeSW11;
-
-	@FXML
-	private VBox spikeSW12;
-
-	@FXML
-	private VBox spikeSW7;
-
-	@FXML
-	private VBox spikeSW8;
-
-	@FXML
-	private VBox spikeSW9;
-
+    @FXML
+	private AnchorPane fullBoard;
+    
+    
 	private List<Node> boardSpikes;
 	
+	
 	/** Adds a piece to the gameboard
-	 * @param p1 The piece node to add to board
-	 * @param spikeNumber The spike the piece is added to 
+	 * @param nodeToAdd The piece node to add to board
+	 * @param fieldNumber The spike the piece is added to
 	 */
-	public void addPieceToField(Node p1, int spikeNumber)
+	public void addPieceToField(Node nodeToAdd, int fieldNumber)
 	{
-		((VBox) boardSpikes.get(spikeNumber)).getChildren().add(p1);
+	    if (fieldNumber >= FIRST_SPIKE_NR && fieldNumber <= LAST_SPIKE_NR)
+	    {
+	        findSpikeGroup(fieldNumber).add(nodeToAdd, internalSpikeGroupPosition(fieldNumber), topSpikePosition(fieldNumber));
+	    }
+	    else 
+	    {
+	        switch(fieldNumber)
+	        {
+	        case BLACK_PRISON: blackPrison.getChildren().add(nodeToAdd); break;
+	        case WHITE_PRISON: whitePrison.getChildren().add(nodeToAdd); break;
+	        case BLACK_HOME: blackHome.getChildren().add(nodeToAdd); break;
+	        case WHITE_HOME: whiteHome.getChildren().add(nodeToAdd); break;
+	        default: throw new IllegalArgumentException(fieldNumber + " is negative or greater than number of fields");
+	        }
+	    }
+	}
+	
+	/** Removes a node (a game piece) from the GridPane of spikes
+	 * @param removee The node to be removed
+	 * @param gridPane The GridPane from which the node will be removed
+	 */
+	public void removePieceFromField(Node removee, GridPane gridPane)
+	{
+	    gridPane.getChildren().remove(removee);
+	}
+	
+	// Helper methods for GridPane handling
+	private int internalSpikeGroupPosition(int fieldNumber)
+	{
+	    int internalSpike = fieldNumber%SPIKE_WIDTH;
+	    if (fieldNumber < FIRST_TOP_SPIKE_NR)
+	    {
+	        internalSpike = Math.abs(internalSpike - (SPIKE_WIDTH - 1));
+	    }
+	    return internalSpike;
 	}
 
-	public List<Node> getBoardSpikes()
+	// TODO Fix when boardState/game class is ready. Responsibility of getting the first available free position lies in the model layer, not here
+	int tempFieldNr = -1;
+	int tempSpikeCount = 0;
+	private int topSpikePosition(int fieldNumber)
 	{
-		return boardSpikes;
+//	    int freeYPosition = currentGame.getBoardState.getNumberOfPieces(fieldNumber)%SPIKE_HEIGHT;
+	    if (tempFieldNr == fieldNumber) tempSpikeCount++;
+	    else
+	    {
+	        tempFieldNr = fieldNumber;
+	        tempSpikeCount = 0;
+	    }
+	    int freeYPosition = tempSpikeCount;
+	    // Reversing the int for bottom spikes to fit the GridPane row ordering
+	    if (fieldNumber < FIRST_TOP_SPIKE_NR) freeYPosition = Math.abs(freeYPosition - (SPIKE_HEIGHT - 1));
+	    return freeYPosition;
 	}
 	
+    private GridPane findSpikeGroup(int spikeNumber)
+    {
+        int spikeGroup = spikeNumber/6;
+        switch(spikeGroup)
+        {
+        case 0: return spikesSE;
+        case 1: return spikesSW;
+        case 2: return spikesNW;
+        case 3: return spikesNE;
+        }
+        throw new IllegalArgumentException("Argument " + spikeNumber + " is greater number of spikes");
+    }
 	
+    
+    
 	List<PieceController> pieceArrayBlack = new ArrayList<PieceController>();
 	List<PieceController> pieceArrayWhite = new ArrayList<PieceController>();
 	
 	/**
-	 * Creating and adding pieces to lists
+	 * Creating and adding pieces to lists 
 	 */
 	public void addPieces(){
-		
-		for(int i = 1; i <= 15; i++) {
-		    pieceArrayWhite.add(new PieceController(true, this));
-		}
-		
-		for(int i = 1; i <= 15; i++) {
-		    pieceArrayBlack.add(new PieceController(false, this));
-		}
-
+//		
+//		for(int i = 1; i <= 2; i++) {
+//		    pieceArrayWhite.add(new PieceController(true, this));
+//		}
+//		
+//		for(int i = 1; i <= 0; i++) {
+//		    pieceArrayBlack.add(new PieceController(false, this));
+//		}
+	    tempPieceInsertion();
 	}
 	
+	
+
 	
 	/** Sets up the game-board for a new game
 	 * 	Clears board, gets method that creates pieces.
@@ -158,68 +165,158 @@ public class BoardController
 	 */
 	public void startBoard()
 	{
-		//loops through each spike and clears them.
-		for (int i = 0; i < 24; i++) {
-			((VBox) boardSpikes.get(i)).getChildren().clear();
-		}
-		
-		//Does not create pieces if they already exist
-		if(!(pieceArrayBlack.size() >=15) && !(pieceArrayWhite.size() >=15)){
+//		//loops through each spike and clears them.
+//		for (int i = 0; i < 24; i++) {
+//			((VBox) boardSpikes.get(i)).getChildren().clear();
+//		}
+//		
+//		//Does not create pieces if they already exist
+//		if(!(pieceArrayBlack.size() >=15) && !(pieceArrayWhite.size() >=15)){
 			addPieces();
-		}
-		
+//		}
+//		
 		//Adds pieces to its designated position
-		addPieceToField(pieceArrayWhite.get(0).getPieceView(), 11);
-		addPieceToField(pieceArrayWhite.get(1).getPieceView(), 11);
-		addPieceToField(pieceArrayWhite.get(2).getPieceView(), 11);
-		addPieceToField(pieceArrayWhite.get(3).getPieceView(), 11);
-		addPieceToField(pieceArrayWhite.get(4).getPieceView(), 11);
-		
-		addPieceToField(pieceArrayWhite.get(5).getPieceView(), 0);
-		addPieceToField(pieceArrayWhite.get(6).getPieceView(), 0);
-		
-		addPieceToField(pieceArrayWhite.get(7).getPieceView(), 18);
-		addPieceToField(pieceArrayWhite.get(8).getPieceView(), 18);
-		addPieceToField(pieceArrayWhite.get(9).getPieceView(), 18);
-		addPieceToField(pieceArrayWhite.get(10).getPieceView(), 18);
-		addPieceToField(pieceArrayWhite.get(11).getPieceView(), 18);
-		
-		addPieceToField(pieceArrayWhite.get(12).getPieceView(), 16);
-		addPieceToField(pieceArrayWhite.get(13).getPieceView(), 16);
-		addPieceToField(pieceArrayWhite.get(14).getPieceView(), 16);
-		
-		addPieceToField(pieceArrayBlack.get(0).getPieceView(), 12);
-		addPieceToField(pieceArrayBlack.get(1).getPieceView(), 12);
-		addPieceToField(pieceArrayBlack.get(2).getPieceView(), 12);
-		addPieceToField(pieceArrayBlack.get(3).getPieceView(), 12);
-		addPieceToField(pieceArrayBlack.get(4).getPieceView(), 12);
-		
-		addPieceToField(pieceArrayBlack.get(5).getPieceView(), 23);
-		addPieceToField(pieceArrayBlack.get(6).getPieceView(), 23);
-		
-		addPieceToField(pieceArrayBlack.get(7).getPieceView(), 5);
-		addPieceToField(pieceArrayBlack.get(8).getPieceView(), 5);
-		addPieceToField(pieceArrayBlack.get(9).getPieceView(), 5);
-		addPieceToField(pieceArrayBlack.get(10).getPieceView(), 5);
-		addPieceToField(pieceArrayBlack.get(11).getPieceView(), 5);
-		
-		addPieceToField(pieceArrayBlack.get(12).getPieceView(), 7);
-		addPieceToField(pieceArrayBlack.get(13).getPieceView(), 7);
-		addPieceToField(pieceArrayBlack.get(14).getPieceView(), 7);
+//		addPieceToField(pieceArrayWhite.get(0).getPieceView(), 11);
+//		addPieceToField(pieceArrayWhite.get(1).getPieceView(), 11);
+//		addPieceToField(pieceArrayWhite.get(2).getPieceView(), 11);
+//		addPieceToField(pieceArrayWhite.get(3).getPieceView(), 11);
+//		addPieceToField(pieceArrayWhite.get(4).getPieceView(), 11);
+//		
+//		addPieceToField(pieceArrayWhite.get(5).getPieceView(), 0);
+//		addPieceToField(pieceArrayWhite.get(6).getPieceView(), 0);
+//		
+//		addPieceToField(pieceArrayWhite.get(7).getPieceView(), 18);
+//		addPieceToField(pieceArrayWhite.get(8).getPieceView(), 18);
+//		addPieceToField(pieceArrayWhite.get(9).getPieceView(), 18);
+//		addPieceToField(pieceArrayWhite.get(10).getPieceView(), 18);
+//		addPieceToField(pieceArrayWhite.get(11).getPieceView(), 18);
+//		
+//		addPieceToField(pieceArrayWhite.get(12).getPieceView(), 16);
+//		addPieceToField(pieceArrayWhite.get(13).getPieceView(), 16);
+//		addPieceToField(pieceArrayWhite.get(14).getPieceView(), 16);
+//		
+//		addPieceToField(pieceArrayBlack.get(0).getPieceView(), 12);
+//		addPieceToField(pieceArrayBlack.get(1).getPieceView(), 12);
+//		addPieceToField(pieceArrayBlack.get(2).getPieceView(), 12);
+//		addPieceToField(pieceArrayBlack.get(3).getPieceView(), 12);
+//		addPieceToField(pieceArrayBlack.get(4).getPieceView(), 12);
+//		
+//		addPieceToField(pieceArrayBlack.get(5).getPieceView(), 23);
+//		addPieceToField(pieceArrayBlack.get(6).getPieceView(), 23);
+//		
+//		addPieceToField(pieceArrayBlack.get(7).getPieceView(), 5);
+//		addPieceToField(pieceArrayBlack.get(8).getPieceView(), 5);
+//		addPieceToField(pieceArrayBlack.get(9).getPieceView(), 5);
+//		addPieceToField(pieceArrayBlack.get(10).getPieceView(), 5);
+//		addPieceToField(pieceArrayBlack.get(11).getPieceView(), 5);
+//		
+//		addPieceToField(pieceArrayBlack.get(12).getPieceView(), 7);
+//		addPieceToField(pieceArrayBlack.get(13).getPieceView(), 7);
+//		addPieceToField(pieceArrayBlack.get(14).getPieceView(), 7);
+	}
+
+
+    @FXML
+    void fireSpikeGroupDragOver(DragEvent dragOver) 
+    {
+     /* TODO This ImageView of piece should be a custom node and maybe visitor pattern should be 
+      * applied if many other types should also be handled by spike groups. */
+        if (dragOver.getGestureSource() instanceof ImageView /* && dragEvent.getDragboard().hasImage() - maybe? */)
+        {
+            dragOver.acceptTransferModes(TransferMode.MOVE);
+        }
+    }
+    
+
+    @FXML
+    void fireSpikeGroupDragDrop(DragEvent dragDrop) 
+    {
+        /* TODO This ImageView of piece should be a custom node and maybe visitor pattern should be 
+         * applied if many other types should also be handled by spike groups. */
+        /* TODO Implement legality of move */
+        if (dragDrop.getGestureSource() instanceof ImageView /* && dragEvent.getDragboard().hasImage() - maybe? */)
+        {
+            GridPane currentSpike = (GridPane) dragDrop.getGestureTarget();
+            double spikeColumnWidth = currentSpike.getWidth()/SPIKE_WIDTH;
+            double mousePosInSpike = dragDrop.getX();
+            int hoverColumnNr = (int) (mousePosInSpike/spikeColumnWidth);
+            // Oh lord, what a mess
+            int tempSpikeGroupNr = tempWhichSpikeGroup(currentSpike);
+            if (tempSpikeGroupNr < 2) hoverColumnNr = Math.abs(hoverColumnNr - (SPIKE_WIDTH - 1));
+            int boardFieldNr = tempSpikeGroupNr*SPIKE_WIDTH + hoverColumnNr;
+            
+            System.out.println("\nSpikeWidth: " + spikeColumnWidth + " Mouse Pos in group: " + mousePosInSpike);
+            System.out.println("Hovering over column: " + hoverColumnNr + " FieldNr on Board: " + boardFieldNr);
+            System.out.print("Spikegroupnr: " + tempWhichSpikeGroup(currentSpike) + "   ");
+            // TODO correct inserting when data can be retrieved from boardstate
+            // TODO Transfer PieceControllers ImageView, not just create from its picture  - addPieceToField((ImageView) dragDrop.getSource(), boardFieldNr);
+            addPieceToField(new ImageView(dragDrop.getDragboard().getImage()), boardFieldNr);
+            dragDrop.setDropCompleted(true);
+        }
+//        dragDrop.consume();   // TODO Consume or not here?
+    }
+    
+	public AnchorPane getFullBoard()
+	{
+	    return fullBoard;
 	}
 	
+	public List<Node> getBoardSpikes()
+	{
+	    return boardSpikes;
+	}
 	
 	@FXML
 	public void initialize()
-	{	
-		boardSpikes = new ArrayList<Node>();
-		boardSpikes.addAll(pieceChamberSE.getChildren());
-		boardSpikes.addAll(pieceChamberSW.getChildren());
-		Collections.reverse(boardSpikes.subList(0, 6)); // Lower 2 chambers are in reversed order of what we need
-		Collections.reverse(boardSpikes.subList(6,12));
-		boardSpikes.addAll(pieceChamberNW.getChildren());
-		boardSpikes.addAll(pieceChamberNE.getChildren());
+	{   
+
 	}
-
-
+	
+	
+	
+	
+	
+	
+	
+	
+    // Temp test methods until game logic is up and running
+    private void tempPieceInsertion()
+    {
+        Map<Integer, Integer> blackMap = new HashMap<Integer, Integer>()
+        {
+            { put(5, 5); put(7, 3); put(12, 5); put(23, 2); }
+        };
+        Map<Integer, Integer> whiteMap = new HashMap<Integer, Integer>()
+        {
+            { put(0, 2); put(11, 5); put(16, 3); put(18, 5); }
+        };
+        PieceController currentPieceController;
+        for (Map.Entry<Integer, Integer> entry: blackMap.entrySet())
+        {
+            for (int i = 0; i<entry.getValue(); i++)
+            {
+                currentPieceController = new PieceController(false, this, entry.getKey());
+                pieceArrayBlack.add(currentPieceController);
+                addPieceToField(currentPieceController.getPieceNode(), entry.getKey());
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry: whiteMap.entrySet())
+        {
+            for (int i = 0; i<entry.getValue(); i++)
+            {
+                currentPieceController = new PieceController(true, this, entry.getKey());
+                pieceArrayBlack.add(currentPieceController);
+                addPieceToField(currentPieceController.getPieceNode(), entry.getKey());
+            }
+        }
+    }
+    
+    private int tempWhichSpikeGroup(GridPane mystery)
+    {
+        if (mystery.equals(spikesSE)) return 0;
+        else if (mystery.equals(spikesSW)) return 1;
+        else if (mystery.equals(spikesNW)) return 2;
+        else if (mystery.equals(spikesNE)) return 3;
+        throw new IllegalArgumentException("Not a known spike group.");
+    }
 }
--- src/controller/PieceController.java
+++ src/controller/PieceController.java
@@ -1,36 +1,58 @@
 package controller;
 
+
 import model.piece.Piece;
 import javafx.scene.Cursor;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.event.EventHandler;
 
+
+
+ /** 
+ *  Controller class of each game piece.
+ *  Handles the connection between the piece model and its representation in the GUI
+ * 
+ * @since 7/5 2014
+ */
 public class PieceController
 {
-
-	// Even though static, this will still load on every new PieceController instantiated, so nothing gained here yet...
+    
+	 /* -- Even though static, this will still load on every new PieceController instantiated, so nothing gained here yet... 
+	 *  ++ This is untrue, a class's static initialisation happens just prior to the very first time it is used,
+	 *  ++ unrelated to whether it's a static call or instantiation of a new object, so this should be fine /Illum */
 	private static final Image whitePiece = new Image("/resources/piece/backgammongreenpiece.png");
 	private static final Image blackPiece = new Image("/resources/piece/backgammonredpiece.png");
 	
 	private static final DropShadow pieceDragShadow = new DropShadow(25.0, 0.0, 0.0, Color.LIME); 
+	private static final DropShadow pieceSimpleDragShadow = new DropShadow();
 	
 	private ImageView pieceView;
+	private Node pieceNode;
+	private BoardController boardController;
 	
 	private Piece piece;
-	private DragInfo dragInfo;
 
 
 	/** Constructor of a new piece
 	 * @param white If piece is white
 	 * @param boardController BoardController
+	 * @param fieldNumber Field number of game board of which the piece is being created upon
 	 */
-	public PieceController(boolean white, BoardController boardController)
+	public PieceController(boolean white, BoardController boardController, int fieldNumber)
 	{
-		piece = new Piece(white, 1); // Where to place these logical pieces and how to connect the ImageView with it, without breaking the model/view/control layers?
+	    this.boardController = boardController;
+		piece = new Piece(white, fieldNumber); // Where to place these logical pieces and how to connect the ImageView with it, without breaking the model/view/control layers?
 		if(white)
 		{
 			pieceView = new ImageView(whitePiece);
@@ -39,76 +61,65 @@ public class PieceController
 		{
 			pieceView = new ImageView(blackPiece);
 		}
-		setupMousePress();
-		setupMouseRelease();
-		setupMouseDrag();
-		consumeRest();
+
+		pieceNode = setupMouseHandlers();
 	}
-	
-	/** Returns the ImageView
-	 * @return The ImageView
-	 */
-	public ImageView getPieceView()
-	{
-		return pieceView;
-	}
-	
-	
+
 	
 	
 	// Next part handles mouse events
 
 	private class DragInfo
 	{
-		public double originalX;
-		public double originalY;
-		
-		public DragInfo(MouseEvent mousePress)
-		{
-			originalX = pieceView.getTranslateX() - mousePress.getX();
-			originalY = pieceView.getTranslateY() - mousePress.getY();
-		}
-
-		public double getOriginalX()
-		{
-			return originalX;
-		}
-
-		public double getOriginalY()
-		{
-			return originalY;
-		}		
-
-//		public double XPosPiece;
-//		public double YPosPiece;
-//    public double Xmouse;
-//    public double Ymouse;
- 
+		private double originalX;
+		private double originalY;
 	}
 
-	int count = 0;
+	int dragEventsCount = 0;
 	
-	private void setupMouseDrag()
+	
+	// TODO Fix mouse handlers. It would seem a drag event eats these mouse events. Are they incompatible?
+	private Node setupMouseHandlers()
 	{
-		pieceView.addEventFilter
+	    final DragInfo dragInfo = new DragInfo();
+	    Group pieceAsGroup = new Group(pieceView);
+//	    Group pieceAsGroup = new Group(pieceNode);
+		pieceAsGroup.addEventFilter
 		(
 				MouseEvent.MOUSE_DRAGGED, 
 				new EventHandler<MouseEvent>()
 				{
 					public void handle(MouseEvent mouseDrag)
 					{
-						pieceView.setTranslateX(dragInfo.originalX + mouseDrag.getX());
-						pieceView.setTranslateY(dragInfo.originalY + mouseDrag.getY());
-						count++;
-						mouseDrag.consume();
+	                    pieceView.translateXProperty().set(dragInfo.originalX + mouseDrag.getX());
+	                    pieceView.translateYProperty().set(dragInfo.originalY + mouseDrag.getY());
+	                    // If node is dragged instead of imageview, heavy jittering will follow
+//					    pieceNode.translateXProperty().set(dragInfo.originalX + mouseDrag.getX());
+//					    pieceNode.translateYProperty().set(dragInfo.originalY + mouseDrag.getY());
+					    dragEventsCount++;
 					}
 				}
 		);
-	}
-	
-	private void setupMouseClick()
-	{
-		pieceView.addEventFilter
+
+		pieceAsGroup.addEventFilter
+		(
+		        MouseEvent.DRAG_DETECTED,
+		        new EventHandler<MouseEvent>()
+		        {
+		            public void handle(MouseEvent dragDetected)
+		            {
+		                Dragboard pieceDragBoard = pieceView.startDragAndDrop(TransferMode.MOVE);
+		                
+		                ClipboardContent pieceAsContent = new ClipboardContent();
+		                pieceAsContent.putImage(pieceView.getImage());
+		                pieceDragBoard.setContent(pieceAsContent);
+		                dragDetected.consume();
+		            }
+
+		        }
+		);         
+		
+		pieceAsGroup.addEventFilter
 		(
 				MouseEvent.MOUSE_CLICKED, 
 				new EventHandler<MouseEvent>()
@@ -119,48 +130,67 @@ public class PieceController
 					}
 				}
 		);
-	}
-	
-	private void setupMousePress()
-	{
-		pieceView.addEventFilter
+
+		pieceAsGroup.addEventFilter
 		(
 				MouseEvent.MOUSE_PRESSED, 
 				new EventHandler<MouseEvent>()
 				{
 					public void handle(MouseEvent mousePress)
 					{
-						pieceView.setCursor(Cursor.NONE);	// Piece "becomes" cursor during press
-						pieceView.setEffect(pieceDragShadow);	
+					    // This node is still below the cursor. By setting node as mouse transparent,
+					    // other nodes underneath will receive mouse events.
+					    pieceNode.setMouseTransparent(true);   
+					    dragEventsCount = 0;
+//					    boardController.transferToBoard(pieceNode, 3);
+//					    boardController.removeNode(pieceNode, 3);
+//						pieceView.setCursor(Cursor.NONE);	// Piece "becomes" cursor during press
+//						pieceView.setEffect(pieceSimpleDragShadow);	// For some reason, during this mouse press event, the piece moves if a shadow is set...
 						pieceView.toFront(); // So piece will move above the rest of gui components, including other pieces
-						dragInfo = new DragInfo(mousePress);
-//						mousePress.consume(); ?
+                        dragInfo.originalX = pieceView.getTranslateX() - mousePress.getX();
+                        dragInfo.originalY = pieceView.getTranslateY() - mousePress.getY();
 					}
 				}
 		);
-	}
-	
-	private void setupMouseRelease()
-	{
-		pieceView.addEventFilter
-		(
-				MouseEvent.MOUSE_RELEASED, 
-				new EventHandler<MouseEvent>()
-				{
-					public void handle(MouseEvent mouseRelease)
-					{
-						pieceView.setCursor(Cursor.OPEN_HAND); // Cursor back to normal
-						pieceView.setEffect(null);	// Removes shadow
-						System.out.println(count);
-//						mouseRelease.consume(); ?
-					}
-				}
-		);
-	}
-	
-	private void consumeRest()
-	{
-		pieceView.addEventFilter
+
+		// TODO Figure out why drag events interferes with MOUSE_RELEASED eventfilter
+//		pieceAsGroup.addEventFilter
+//		(
+//				MouseEvent.MOUSE_RELEASED, 
+//				new EventHandler<MouseEvent>()
+//				{
+//					public void handle(MouseEvent mouseRelease)
+//					{
+//					    // Mouse transparency set to false again to make node visible for new events.
+//					    pieceNode.setMouseTransparent(false);
+//						pieceView.setCursor(Cursor.OPEN_HAND); // Cursor back to normal
+//						pieceView.setEffect(null);	// Removes shadow
+////						boardController.addPieceToField(pieceNode, 3);
+//						System.out.println("number of drag events: " + dragEventsCount);
+//					}
+//				}
+//		);
+
+	       pieceAsGroup.addEventFilter
+	        (
+	                DragEvent.DRAG_DONE, 
+	                new EventHandler<DragEvent>()
+	                {
+	                    public void handle(DragEvent dragDone)
+	                    {
+	                        // Mouse transparency set to false again to make node visible for new events.
+	                        pieceNode.setMouseTransparent(false);
+	                        pieceView.setCursor(Cursor.OPEN_HAND); // Cursor back to normal
+//	                        pieceView.setEffect(null);  // Removes shadow
+	                        boardController.removePieceFromField(pieceNode, (GridPane) pieceView.getParent().getParent());
+	                        System.out.println("number of drag events: " + dragEventsCount);
+	                    }
+
+	                }
+	        );
+		        
+     
+		pieceAsGroup.addEventFilter
 		(
 				MouseEvent.ANY, 
 				new EventHandler<MouseEvent>()
@@ -171,7 +201,21 @@ public class PieceController
 					}
 				}
 		);
+		
+		return pieceAsGroup;
 	}
+
+
+    public Piece getPiece()
+    {
+        return piece;
+    }
+
+
+    public Node getPieceNode()
+    {
+        return pieceNode;
+    }
 	
 	
 }
